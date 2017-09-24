7a359a6bea0bdc54375e14c79a2fba1efd166e43
isurunix <isurunix@gmail.com>
2017-09-24 14:33:11 +0530

    v1.0.0

---
+++ tomcat-install-script.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+TOMCAT_URL="http://www-eu.apache.org/dist/tomcat/tomcat-8/v8.0.46/bin/apache-tomcat-8.0.46.tar.gz"
+
+function validate_java_home {
+    if [ -z ${JAVA_HOME} ]
+    then
+        # do something if the file doesn't provide ${JAVA_HOME}
+	echo 'Could not find JAVA_HOME. Please set JAVA_HOME'
+	exit
+    else 
+	echo 'JAVA_HOME found: '$JAVA_HOME
+        if [ ! -e ${JAVA_HOME} ]
+        then
+            # do something if the file provides a non existent ${JAVA_HOME}
+	    echo 'Invalid JAVA_HOME'
+	    exit
+        fi
+    fi
+}
+
+echo 'Installin tomcat server...'
+echo 'Checking for JAVA_HOME...'
+validate_java_home
+
+echo 'Downloading tomcat-8.0...'
+if [ ! -f /etc/apache-tomcat-8*tar.gz ]
+then
+    curl -O $TOMCAT_URL
+fi
+echo 'Finished downloading..'
+
+echo 'Creating install directories'
+sudo mkdir -p '/opt/tomcat/8_0'
+
+if [ -d "/opt/tomcat/8_0" ]
+then
+    echo 'Extracting content to install directory..'
+    sudo tar xzf apache-tomcat-8*tar.gz -C "/opt/tomcat/8_0" --strip-components=1
+    echo 'Creating tomcat user group...'
+    sudo groupadd tomcat
+    sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+    
+    echo 'Setting file permissions...'
+    cd "/opt/tomcat/8_0"
+    sudo chgrp -R tomcat "/opt/tomcat/8_0"
+    sudo chmod -R g+r conf
+    sudo chmod -R g+x conf
+
+    # This should be commented out on a production server
+    sudo chmod -R g+w conf
+
+    sudo chown -R tomcat webapps/ work/ temp/ logs/
+
+    echo 'Setting up tomcat service...'
+    sudo touch tomcat.service
+    sudo chmod 777 tomcat.service 
+    echo "[Unit]" > tomcat.service
+    echo "Description=Apache Tomcat Web Application Container" >> tomcat.service
+    echo "After=network.target" >> tomcat.service
+
+    echo "[Service]" >> tomcat.service
+    echo "Type=forking" >> tomcat.service
+
+    echo "Environment=JAVA_HOME=$JAVA_HOME" >> tomcat.service
+    echo "Environment=CATALINA_PID=/opt/tomcat/8_0/temp/tomcat.pid" >> tomcat.service
+    echo "Environment=CATALINA_HOME=/opt/tomcat/8_0" >> tomcat.service
+    echo "Environment=CATALINA_BASE=/opt/tomcat/8_0" >> tomcat.service
+    echo "Environment=CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC" >> tomcat.service
+    echo "Environment=JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom" >> tomcat.service
+
+    echo "ExecStart=/opt/tomcat/8_0/bin/startup.sh" >> tomcat.service
+    echo "ExecStop=/opt/tomcat/8_0/bin/shutdown.sh" >> tomcat.service
+
+    echo "User=tomcat" >> tomcat.service
+    echo "Group=tomcat" >> tomcat.service
+    echo "UMask=0007" >> tomcat.service
+    echo "RestartSec=10" >> tomcat.service
+    echo "Restart=always" >> tomcat.service
+
+    echo "[Install]" >> tomcat.service
+    echo "WantedBy=multi-user.target" >> tomcat.service
+
+    sudo mv tomcat.service /etc/systemd/system/tomcat.service
+    sudo chmod 755 /etc/systemd/system/tomcat.service
+    sudo systemctl daemon-reload
+    
+    echo 'Starting tomcat server....'
+    sudo systemctl start tomcat
+    exit
+else
+    echo 'Could not locate installation direcotry..exiting..'
+    exit
+fi
